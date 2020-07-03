05407533db7516eb6ea9258bacac4be2a1d0ca81
Sameer Argade <sameer.argade@dws.com.au>
2020-07-03 18:30:54 +1000

    Added code to implement Gateway Logic

--- src/main/java/com/argade/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/argade/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,23 @@ package com.argade.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
